c335b2428762a3a92decc918734c901bbcae446e
Alfred Francis <60069653+alfredfrancis68@users.noreply.github.com>
2021-09-02 22:15:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+//console.log('Hey this is my first piece of JS code !!! YAY')
+
+//console.log('123456878')
+
+//console.log({ name: "Alfred" })
+
+//console.log(true)
+
+//console.log(false)
+
+//console.log(false, 1234456)
+
+//console.log(true, 23453, 'string', { name: "ALfred" })
+
+//console.warn('This is a Warning')
+
+//console.error('This is an error')
+
+// Data types in JS
+
+//Primitive datatype
+var Name = "Alfred Francis"
+Name = "Alfred"
+console.log(Name)
+
+let age = 19;
+console.log(age)
+
+let age1 = 123454667;
+console.log(age1)
+
+let age2 = 213.34556;
+let Name2 = 'asdfgkhkjrf';
+console.log(typeof Name2)
+
+let brand = null
+console.log(typeof brand)
+
+
+//Non-Primitive datatype
+
+let arr = [1, 2, 3, 4, 5]
+console.log(arr)
+
+let arr1 = [1, 2, 3, 4, 5]
+arr1.push("value")
+arr1.push(false)
+arr1.unshift(false)
+arr1.unshift(021)
+console.log(arr1)
+console.log(typeof arr1)
+
+//  Coercion in JS
+
+let Name3 = "Alfred"
+let age3 = 21
+console.log(Name3.toUpperCase())
+    //console.log(age3.)
+
+let Name3 = "Alfred"
+let age3 = 21
+console.log(Name3.toUpperCase())
+    //console.log(age3.)
