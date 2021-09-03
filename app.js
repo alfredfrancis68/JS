628da9a2f6cbc89de64766c2af7e953a5d8904b8
alfred francis <alfred.francis68@gmail.com>
2021-09-03 20:03:51 +0530

    JS minor commit

--- app.js
+++ app.js
@@ -19,80 +19,145 @@
 // Data types in JS
 
 //Primitive datatype
-var Name = "Alfred Francis"
-Name = "Alfred"
-console.log(Name)
+// var Name = "Alfred Francis"
+// Name = "Alfred"
+// console.log(Name)
 
-let age = 19;
-console.log(age)
+// let age = 19;
+// console.log(age)
 
-let age1 = 123454667;
-console.log(age1)
+// let age1 = 123454667;
+// console.log(age1)
 
-let age2 = 213.34556;
-let Name2 = 'asdfgkhkjrf';
-console.log(typeof Name2)
+// let age2 = 213.34556;
+// let Name2 = 'asdfgkhkjrf';
+// console.log(typeof Name2)
 
-let brand = null
-console.log(typeof brand)
+// let brand = null
+// console.log(typeof brand)
 
 
-//Non-Primitive datatype
+// //Non-Primitive datatype
 
-let arr = [1, 2, 3, 4, 5]
-console.log(arr)
+// let arr = [1, 2, 3, 4, 5]
+// console.log(arr)
 
-let arr1 = [1, 2, 3, 4, 5]
-arr1.push("value")
-arr1.push(false)
-arr1.unshift(false)
-arr1.unshift(021)
-console.log(arr1)
-console.log(typeof arr1)
+// let arr1 = [1, 2, 3, 4, 5]
+// arr1.push("value")
+// arr1.push(false)
+// arr1.unshift(false)
+// arr1.unshift(021)
+// console.log(arr1)
+// console.log(typeof arr1)
 
-//  Coercion in JS
-
-let Name3 = "Alfred"
-let age3 = 21
-console.log(Name3.toUpperCase())
-    //console.log(age3.)
+// //  Coercion in JS
 
 // let Name3 = "Alfred"
 // let age3 = 21
 // console.log(Name3.toUpperCase())
-// console.log(age3.)
+//     //console.log(age3.)
 
-const specific_name = "Alfred"
-console.log(specific_name)
+// // let Name3 = "Alfred"
+// // let age3 = 21
+// // console.log(Name3.toUpperCase())
+// // console.log(age3.)
+
+// const specific_name = "Alfred"
+// console.log(specific_name)
+
+//-------------------------------------
+
+// // ARITHMETIC OPERATORS
 
 
-// ARITHMETIC OPERATORS
+// const a = 2
+// const b = 15
+// console.log(a + b)
+
+// console.log(a - b)
+
+// console.log(a * b)
+
+// console.log(a / b)
+
+// console.log(a ** b)
+
+// console.log(b % a)
+
+// console.log('--------------------------------')
+
+// let c = 5
+// c += 1 // Equivalent to   c = c + 1    or   c++ 
+// console.log(c)
+
+// let d = 10
+// d *= 10 //c*=10 is equivalent to  d = d * 10
+// console.log(d)
+
+// let e = 12
+// 3 /= 12 //d/=12 is equivalent to  e = e / 12
+// console.log(e)
+
+// const a = '2'
+// const b = 2
+// console.log(a - b)
+
+//----------------------------------------------------------
+
+//ASSIGNMENT OPERATORS
+
+// let name = 'Alfred'
+// let a = 10
+// a += 9
+// console.log(a)
 
 
+// let name = 'alfred'
+// let a = 19
+// a = a + 9
+// console.log(a)
+// console.log(a += name)
+
+//---------------------------------------
+
+//COMPARISON OPERATORS
 const a = 2
-const b = 15
-console.log(a + b)
 
-console.log(a - b)
+const b = 3
 
-console.log(a * b)
+const c = 10
 
-console.log(a / b)
+console.log('a>b', a > b)
+console.log('a<b', a < b)
+console.log('a==c', a == c)
+console.log('a>=b', a >= b)
+console.log('a>=c', a >= c)
 
-console.log(a ** b)
+console.log('--------------------')
 
-console.log(b % a)
+//--------------------------------
 
-console.log('--------------------------------')
+const e = 11
 
-let c = 5
-c += 1 // Equivalent to   c = c + 1    or   c++ 
-console.log(c)
+const f = '11'
+console.log(e == f)
 
-let d = 10
-d *= 10 //c*=10 is equivalent to  d = d * 10
-console.log(d)
 
-let e = 12
-3 /= 12 //d/=12 is equivalent to  e = e / 12
-console.log(e)
+console.log('---------------------')
+
+const g = 'alfred '
+const h = 'Alfred'
+
+console.log(g == h)
+
+console.log('---------------------')
+
+const i = 23
+const j = '23'
+
+console.log(i == j)
+
+console.log('-----------------------')
+
+
+console.log(true && false)
