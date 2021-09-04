d042797fd95fb6f7a9701e60cb6997a7fdb9980b
alfred francis <alfred.francis68@gmail.com>
2021-09-04 19:03:34 +0530

    DOM selection methods

--- app.js
+++ app.js
@@ -18,3 +18,23 @@
 
 // const element = document.querySelector('.heading')
 // console.log(typeof element)
+
+
+//console.log('-----------------------------------------------------')
+
+
+// const element = document.getElementById("selectme");
+
+// const div = document.getElementsByTagName("div");
+
+// const element = document.getElementById("selectMe");
+
+// const elementClass = document.getElementsByClassName("heading");
+
+// const elementTag = document.getElementsByTagName("h2");
+
+// const elementSelector = document.querySelector(".heading");
+
+// const elementSelectorAll = document.querySelectorAll(".heading");
+
+// console.log(elementSelectorAll);
