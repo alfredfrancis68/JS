cb85544627e1e7ae6da15b599f8c8cf52deeb6a0
alfred francis <alfred.francis68@gmail.com>
2021-09-06 18:25:42 +0530

    Function keyword

---
+++ app.js
@@ -0,0 +1,88 @@
+// const rock = document.querySelector('#rock')
+// console.log(rock)
+// const rock = document.querySelector('#paper')
+// console.log(paper)
+// const rock = document.querySelector('#scissor')
+// console.log(scissor)
+
+// function logName(name) {
+//     console.log("Nishta")
+// }
+
+// logName()
+
+// function logName(name) {
+//     console.log(name)
+// }
+
+// logName('asdfvtafv')
+
+
+// var Name
+
+// function logName(name) {
+//     console.log(name)
+// }
+// logName()
+
+
+
+// function logName(Name = "Alfred Francis", age = 18) {
+//     console.log(Name + ", you are " + age)
+// }
+// logName()
+// Never misspell the funciton keyword.
+// A function can have multiple parameters.
+// You can set default values for the parameters.
+//Any variable you define inside a function's body is local to that function
+
+
+
+// function logName(Name = "Alfred Francis", age = 18) {
+//     console.log(Name + ", you are " + age)
+// }
+// function add(a = 0, b = 0) {
+//     const sum = a + b
+//     console.log(a + b)
+// }
+// console.log(sum)
+// const result = add(12, 99)
+// const result = add(12, 99)
+
+
+// function logName(Name = "Alfred Francis", age = 18) {
+//     console.log(Name + ", you are " + age)
+// }
+
+// function add(a = 0, b = 0) {
+//     return a + b
+
+// }
+// const result = add()
+
+// console.log(result)
+
+// function logName(Name = "Alfred Francis", age = 18) {
+//     console.log(Name + ", you are " + age)
+// }
+
+
+
+// function add(a = 0, b = 0) {
+//     if (action == 'add')
+// }
+// const result = add()
+
+// console.log(result)
+// function logName(Name = "Alfred Francis", age = 18) {
+//     console.log(Name + ", you are " + age)
+// }
+
+
+
+// function add(a = 0, b = 0, action = 'add') {
+//     if (action == 'add')
+// }
+// const result = add()
+
+// console.log(result)
