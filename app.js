2117731a164c019d0c43ab63b1721d45c90b2bb4
alfred francis <alfred.francis68@gmail.com>
2021-09-06 18:47:52 +0530

    ES-6 Functions

--- app.js
+++ app.js
@@ -5,62 +5,18 @@
 // const rock = document.querySelector('#scissor')
 // console.log(scissor)
 
-// function logName(name) {
-//     console.log("Nishta")
-// }
-
-// logName()
-
-// function logName(name) {
-//     console.log(name)
-// }
-
-// logName('asdfvtafv')
-
-
-// var Name
-
-// function logName(name) {
-//     console.log(name)
-// }
-// logName()
 
 
 
 // function logName(Name = "Alfred Francis", age = 18) {
 //     console.log(Name + ", you are " + age)
 // }
-// logName()
-// Never misspell the funciton keyword.
-// A function can have multiple parameters.
-// You can set default values for the parameters.
-//Any variable you define inside a function's body is local to that function
 
-
-
-// function logName(Name = "Alfred Francis", age = 18) {
-//     console.log(Name + ", you are " + age)
-// }
-// function add(a = 0, b = 0) {
-//     const sum = a + b
-//     console.log(a + b)
-// }
-// console.log(sum)
-// const result = add(12, 99)
-// const result = add(12, 99)
-
-
-// function logName(Name = "Alfred Francis", age = 18) {
-//     console.log(Name + ", you are " + age)
-// }
-
-// function add(a = 0, b = 0) {
+// const add = function(a = 0, b = 0) {
 //     return a + b
 
 // }
-// const result = add()
-
-// console.log(result)
+// console.log(add (12, 23 ))
 
 
 
@@ -68,22 +24,41 @@
 // function logName(Name = "Alfred Francis", age = 18) {
 //     console.log(Name + ", you are " + age)
 // }
-// function add(a = 0, b = 0) {
-//     if (action == 'add')
+
+// const add = (a = 0, b = 0) => {
+//     return a + b
+
 // }
-// const result = add()
-
-// console.log(result)
+// console.log(add(12, 23))
 
 
 
-// function logName(Name = "Alfred Francis", age = 18) {
-//     console.log(Name + ", you are " + age)
+
+// console.log(add(12, 23))
+
+// function(a = 0, b = 0) {
+//     return a + b
+
 // }
 
-// function add(a = 0, b = 0, action = 'add') {
-//     if (action == 'add')
-// }
-// const result = add()
 
-// console.log(result)
+
+// console.log(add(12, 23))
+
+// const add = function(a = 0, b = 0) {
+//     return a + b
+
+// }
+
+
+
+
+function logName(Name = "Alfred Francis", age = 18) {
+    console.log(Name + ", you are " + age)
+}
+
+const add = function(a = 0, b = 0) => {
+    return a + b
+
+}
+console.log(add(12, 23))
