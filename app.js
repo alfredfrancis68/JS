10ef66cd8cdee111a6881272043f17462513edea
alfred francis <alfred.francis68@gmail.com>
2021-09-07 17:35:21 +0530

    For & while Loops

--- app.js
+++ app.js
@@ -1,5 +1,68 @@
-//for loop
+//     FOR LOOP  -----------------------------------------
+
+
 // let i
 // for (i = 1; i <= 1; i++) {
 //     console.log('iteration' + i)
 // }
+
+
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log("Iteration" + i)
+// }
+// console.log(i)
+
+
+
+// for (var i = 1; i <= 10; i++) {
+//     age = "19"
+//     console.log("Iteration" + i)
+// }
+// console.log(i)
+
+
+
+
+// for (var i = 1; i <= 10; i++) {
+//     var age = "19"
+//     console.log("Iteration" + i)
+// }
+// console.log(age)
+
+
+
+
+// const temp = () => {
+//     var dontknow = "dontknow"
+// }
+// console.log(dontknow)
+
+
+
+
+// const temp = () => {
+//     var dontknow = "dontknow"
+// }
+// temp()
+// console.log(dontknow)
+
+
+
+
+// function temp = () => {
+//     var dontknow = "dontknow"
+// }
+// console.log(dontknow)
+
+
+
+//   WHILE LOOP ------------------------------------------
+
+
+let iteration = 1
+
+while (iteration < 10) {
+    console.log(iteration)
+    iteration++
+}
