5ee09f25afb272d123ee715e69572fd86133a7e2
alfred francis <alfred.francis68@gmail.com>
2021-09-08 16:24:27 +0530

    js commit

--- app.js
+++ app.js
@@ -33,13 +33,13 @@
 // const body = document.querySelector('body')
 
 
-// balls.forEach((ball, iteration) => {
-//     const value = iteration * 50
-//     ball.style = `transform:translateY(${value}px)`
-// })
+balls.forEach((ball, iteration) => {
+    const value = iteration * 50
+    ball.style = `transform:translateY(${value}px)`
+})
 
-// let age = 19
-// let Name = "Sakshi"
+let age = 19
+let Name = "Sakshi"
 
-// console.log('Your age is ' + age)
-// console.log(`Hi ${Name} Your age is ${age}`)
+console.log('Your age is ' + age)
+console.log(`Hi ${Name} Your age is ${age}`)
