8251691b7c22bd254ae3de059cd44cccf775c29c
alfred francis <alfred.francis68@gmail.com>
2021-09-19 08:08:50 +0530

    NodeJS things

---
+++ NodeJS/index.js
@@ -0,0 +1,5 @@
+console.log(__dirname); //Global object
+
+console.log(__filename); //Globalobject
+
+module.exports = someFunction(); //Used to export a module
--- app.js
+++ app.js
@@ -1,43 +1,51 @@
-console.log('Im Alfred Francis.');
+// console.log('Im Alfred Francis.');
 
-console.log({ name: "ALfred Francis" });
+// console.log({ name: "ALfred Francis" });
 
-console.log(123456789);
-console.log(true);
+// console.log(123456789);
+// console.log(true);
 
-console.log(false);
+// console.log(false);
 
-console.log(false, 234243, 'string', { name: "Alfred" });
+// console.log(false, 234243, 'string', { name: "Alfred" });
 
-console.log(false, true, { name: "Alfred" }, "String");
+// console.log(false, true, { name: "Alfred" }, "String");
 
-console.warn("This is warning.");
+// console.warn("This is warning.");
 
-console.error("This is an error.")
+// console.error("This is an error.")
 
-console.log({
-    name: "Alfred Fra-----------ncis",
-    print: () => { 'Friends' }
-})
+// console.log({
+//     name: "Alfred Fra-----------ncis",
+//     print: () => { 'Friends' }
+// })
 
-console.log({ name: "Francis." })
+// console.log({ name: "Francis." })
 
-console.log({ Name: "Alfred Francis." })
+// console.log({ Name: "Alfred Francis." })
 
-console.log({ name: "Alfred Franacois." })
+// console.log({ name: "Alfred Franacois." })
 
-console.log("Alfred Francis-------------")
+// console.log("Alfred Francis-------------")
 
-console.log({
-    name: "Albatroosssss"
-})
+// console.log({
+//     name: "Albatroosssss"
+// })
 
-console.log({
-    name: "Fernandes",
-    print: () => { 'hi' }
-})
+// console.log({
+//     name: "Fernandes",
+//     print: () => { 'hi' }
+// })
+
+// const val = {
+//     name: "Alfred Francis",
+//     print: () => { "hi" }
+// }
+
 
 const val = {
-    name: "Alfred Francis",
-    print: () => { "hi" }
+    firstname: "Alfred",
+    Lastname: "Francis",
+    age: 20,
+    print: () => {}
 }
