e2385e741d3e529fbbf0e95cc4fdc3806c45e71c
alfred francis <alfred.francis68@gmail.com>
2021-09-23 22:48:05 +0530

    getElementByTagName

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-// const element = document.getElementById('selectme');
+// const element = document.getElementById('selectme');//ID SELECTOR
 
-const element = document.getElementsByClassName('Heading');
+
+// const element = document.getElementsByClassName('Heading'); //CLASSNAME SELECTOR
+// console.log(element);
+// console.log(typeof element);
+
+
+const element = document.getElementsByTagName('h2'); //TAGNAME SELECTOR
 console.log(element);
-console.log(typeof element);
