0bb10cb5e8ea93786a7b14374bafe92a6cec9a02
alfred francis <alfred.francis68@gmail.com>
2021-09-24 20:45:37 +0530

    app.js - ROCK-PAPER-SCISSOR

--- app.js
+++ app.js
@@ -2,81 +2,22 @@ const rock = document.querySelector('#rock')
 const paper = document.querySelector('#paper')
 const scissor = document.querySelector('#scissor')
 const board = document.querySelector("#board")
-
-switch (action) {
-    case "rock":
-        board.innerHTML = "Rock crushes scissors"
-    case "paper":
-        board.innerHTML = "Paper traps Rock"
-    case "scissor":
-        board.innerHTML = "Scissor cuts paper"
-    default:
-        board.innerHTML = "Click something"
+const handler = (e) => {
+    const action = e.target.id
+    switch (action) {
+        case 'rock':
+            board.innerHTML = "Rock crushes scissors"
+            break
+        case 'paper':
+            board.innerHTML = "Paper traps Rock"
+            break
+        case 'scissor':
+            board.innerHTML = "Scissor cuts paper"
+            break
+        default:
+            board.innerHTML = "Click something"
+    }
 }
-
-
 rock.addEventListener('click', handler)
 paper.addEventListener('click', handler)
 scissor.addEventListener('click', handler)
-
-
-
-// const handler = (e) => {
-//     const action = e.target.id
-//     if (action === 'rock') {
-//         board.innerHTML = "Rock crushes scissor"
-//     } else if (action === 'paper') {
-//         board.innerHTML = "Paper traps rock"
-//     } else if (action === 'scissor') {
-//         board.innerHTML = "Scissor cuts paper"
-//     } else {
-//         board.innerHTML = "Click something"
-//     }
-// }
-
-// rock.addEventListener('click', handler)
-// paper.addEventListener('click', handler)
-// scissor.addEventListener('click', handler)
-
-
-// const handler = (event) => {
-//     const action = event.target.id
-//     if (action === 'rock') {
-//         console.log("Rock crushes scissor")
-//     } else if (action == 'paper') {
-//         console.log("Paper traps rock")
-//     } else if (action === 'scissor') {
-//         console.log("Scissor cuts paper")
-//     } else {
-//         console.log("Click something")
-//     }
-// }
-
-// rock.addEventListener('click', handler)
-// paper.addEventListener('click', handler)
-// scissor.addEventListener('click', handler)
-
-
-
-// const rock = document.querySelector('#rock')
-// const paper = document.querySelector('#paper')
-// const scissor = document.querySelector('#scissor')
-
-
-// const handler = (event) => {
-//     const action = event.target.id
-//     if (action === 'rock') {
-//         console.log("Rock crushes scissors")
-//     } else if (action === 'paper') {
-//         console.log("Paper traps Rock")
-//     } else if (action === 'scissor') {
-//         console.log("Scissor cuts paper")
-//     } else {
-//         console.log("Click something")
-//     }
-// }
-
-
-// rock.addEventListener('click', handler)
-// paper.addEventListener('click', handler)
-// scissor.addEventListener('click', handler)
