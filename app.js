c6d00f7be842e1cbea2a6074cbe34b43c2a74f05
alfred francis <alfred.francis68@gmail.com>
2021-09-24 20:55:46 +0530

    setTimeout in JS

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
-const rock = document.querySelector('#rock')
-const paper = document.querySelector('#paper')
-const scissor = document.querySelector('#scissor')
-const board = document.querySelector("#board")
-const handler = (e) => {
-    const action = e.target.id
-    switch (action) {
-        case 'rock':
-            board.innerHTML = "Rock crushes scissors"
-            break
-        case 'paper':
-            board.innerHTML = "Paper traps Rock"
-            break
-        case 'scissor':
-            board.innerHTML = "Scissor cuts paper"
-            break
-        default:
-            board.innerHTML = "Click something"
-    }
-}
-rock.addEventListener('click', handler)
-paper.addEventListener('click', handler)
-scissor.addEventListener('click', handler)
+console.log("One")
+
+setTimeout(() => {
+    console.log('I ran out after 5 seconds')
+}, 5000)
+
+console.log("Three")
+
+
+
+
+console.log("One")
+setTimeout(() => {
+    console.log('Two')
+}, 6000)
+
+console.log("Three")
